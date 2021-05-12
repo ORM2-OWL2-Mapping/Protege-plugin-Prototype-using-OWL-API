933cd149824a501e451a03369baf07ee0696829d
DimaDontsov <Dimayahoo@mail.ru>
2021-05-12 11:12:47 +0300

    изменение консольного приложения, чтобы корректно собирался и запускался

--- OWL_API_Console_Program/src/main/java/Main.java
+++ OWL_API_Console_Program/src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     private static OntologyBuilder ontologyBuilder = new OntologyBuilder();
 
     public static void main(String args[ ]) throws Exception {
-        ontologyBuilder.createOntology();
+        ontologyBuilder.loadOntology();
     }
 }
