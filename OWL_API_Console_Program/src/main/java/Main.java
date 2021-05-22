2917c4377002a8724d2a44aeb21965df0a2d6a62
DimaDontsov <Dimayahoo@mail.ru>
2021-05-22 13:10:36 +0300

    Добавил GUI для тестирования

--- OWL_API_Console_Program/src/main/java/Main.java
+++ OWL_API_Console_Program/src/main/java/Main.java
@@ -5,6 +5,9 @@ public class Main {
     private static OntologyBuilder ontologyBuilder = new OntologyBuilder();
 
     public static void main(String args[ ]) throws Exception {
-        ontologyBuilder.loadOntology();
+        //ontologyBuilder.loadOntology();
+
+        GUIForm gui = new GUIForm();
+        gui.enableVisible();
     }
 }
